88d2da58034d8efec01bb5c4aef7b1eb0ae2b59f
Saneyee Sarkar <saneyeesarkar@gmail.com>
2017-10-04 23:51:14 -0700

    hides the checkboxes from html

--- js/scripts.js
+++ js/scripts.js
@@ -97,7 +97,8 @@ $(document).ready(function() {
     });
 
     $("#b1, #b2, #b3, #b4, #b5, #b6, #b7, #b8, #b9, #b10, #b11, #b12, #b13, #b14, #b15, #b16, #b17, #b18, #b19, #b20, #b21, #b22, #b23, #b24, #b25, #b26, #b27, #b28, #b28, #b29, #b30, #b31, #b32, #b33, #b34, #b35, #b36").click(function() {
-      // $(this.id).find("input[type=checkbox]").attr("checked",true);
+
+      $(this).find("input[type=checkbox]").attr("checked",true);
       var smtg2 = this.id;
       var number = parseInt(smtg2.replace("b",""));
       debugger;
@@ -117,8 +118,8 @@ $(document).ready(function() {
     });
 
 $("#a1, #a2, #a3, #a4, #a5, #a6, #a7, #a8, #a9, #a10, #a11, #a12, #a13, #a14, #a15, #a16, #a17, #a18, #a19, #a20, #a21, #a22, #a23, #a24, #a25, #a26, #a27, #a28, #a28, #a29, #a30, #a31, #a32, #a33, #a34, #a35, #a36").click(function() {
-
-  // $(this.id).find("input[type=checkbox]").removeAttr("checked",true);
+ 
+  $(this).find("input[type=checkbox]").attr("checked",true);
   var smtg = this.id;
   var number = parseInt(smtg.replace("a",""));
   if(index === 1 && board1[number-1] != 2){
